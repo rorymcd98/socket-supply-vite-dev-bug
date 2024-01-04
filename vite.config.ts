057d7a6826158d87aee8f442c74d1354870668d4
Rory McDonald <rorymcd98@gmail.com>
2024-01-04 17:49:31 +0000

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import externalize from 'vite-plugin-externalize-dependencies'
+
+export default defineConfig({
+  plugins: [
+    externalize({
+      externals: [/^socket:.*/],
+    }),
+    react(),
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: [/^socket:.*/],
+    },
+  },
+})
